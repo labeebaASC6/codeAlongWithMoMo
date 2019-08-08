8c839b78ff58524e00cb58b2083256a31e4b7dd2
unknown <labeebabrar222@gmail.com>
2019-08-08 11:59:29 -0400

    Set picture

--- libs/main.js
+++ libs/main.js
@@ -1,3 +1,7 @@
+
+// proGamerMove();
+
+
 let url = "https://pokeapi.co/api/v2/pokemon/chimchar";
 
 fetch(url)
@@ -9,9 +13,12 @@ fetch(url)
     let types = getTypes(data);
     let moves = getMoves(data);
     let abilities = getAbilities(data);
-    let chimchar = new Pokemon(name, number, types, moves , abilities);
+    let sprite = data.sprites.front_default;
+    let flavorText = getFlavorText(data);
+    let chimchar = new Pokemon(name, number, types, moves , abilities, sprite, flavorText);
     console.log(chimchar);
     createPokeElement(chimchar);
+    // document.body.style.backgroundImage = `url(${data.sprites.front_default})`
 })
 .catch(function(error){ console.log(error)});
 function getTypes(pokeJSON)
@@ -43,6 +50,8 @@ function getAbilities(pokeJSON)
 }
 function createPokeElement(pokemon)
 {
+    let img = document.createElement("img");
+    img.setAttribute("src", pokemon.sprite);
 
     //do h1 for name
     let h1 = document.createElement("h1");
@@ -70,6 +79,54 @@ function createPokeElement(pokemon)
     }
     //shove zhe pokymone into a box and post it to html without 'fragile' tag
     let div = document.createElement("div");
-    div.append(h1, h2, p, moveUl, ablityUl);
+    div.append(img, h1, h2, p, moveUl, ablityUl);
     document.body.appendChild(div);
+}
+function getFlavorText(pokeJSON)
+{
+    let flavorURL = pokeJSON.species.url;
+    fetch(flavorURL)
+    .then((resp) => resp.json())
+    .then((data) => {
+        let text;
+        let flavorTextArray = data.flavor_text_entries;
+        for(let flavor_text of flavorTextArray)
+        {
+            console.log(flavor_text);
+        }
+    })
+    .catch((err) => console.log(err));
+}
+
+
+
+
+
+let urls = "https://pokeapi.co/api/v2/pokemon/";
+function proGamerMove()
+{
+    var max = 800;
+for(let i = 0; i < max; i++)
+{
+    xd(i + 1);
+}
+}
+function xd(pokeID)
+{
+var thisurl = `${urls}${pokeID}`;
+fetch(thisurl)
+.then((resp) => resp.json())
+.then(function(data) {
+    console.log(data);
+    let name = data.name;
+    let number = data.id;
+    let types = getTypes(data);
+    let moves = getMoves(data);
+    let abilities = getAbilities(data);
+    let chimchar = new Pokemon(name, number, types, moves , abilities);
+    console.log(chimchar);
+    createPokeElement(chimchar);
+    // document.body.style.backgroundImage = `url(${data.sprites.front_default})`
+})
+.catch(function(error){ console.log(error)});
 }
--- libs/pokemon.js
+++ libs/pokemon.js
@@ -1,11 +1,13 @@
 class Pokemon
 {
-    constructor(name, number, types, moves, abilities)
+    constructor(name, number, types, moves, abilities, sprite, flavorText)
     {
         this.name = name;
         this.number = number;
         this.types = types;
         this.moves = moves;
         this.abilities = abilities;
+        this.sprite = sprite;
+        this.flavorText = flavorText;
     }
 }
